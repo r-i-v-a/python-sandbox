f78823f1e754e0447bddbfe110be621a0e719f36
Riva Nathans <rnathans-cf@dnanexus.com>
2021-03-27 19:26:05 +0100

    Iterate through case inputs.

--- src/main.py
+++ src/main.py
@@ -2,9 +2,18 @@
 
 import sys
 
+def calculateCost(caseId, caseList):
+    print("--> Case id", caseId)
+    for x in caseList:
+        print(x)
+
 def main():
-    cases = input()
-    print("--> Number of cases", cases)
+    cases = int(input())
+
+    for i in range(cases):
+        caseLen = int(input())
+        caseList = map(int, input().split(" "))
+        calculateCost(i + 1, caseList)
 
 if __name__ == "__main__":
     main()
